fb8e046bbd0299d16d6a846b087f610aacd04b74
etsubeta132 <etsubdinkawoke@gmail.com>
2024-03-19 11:52:58 +0300

    update test practice

--- test_product.py
+++ test_product.py
@@ -1,37 +1,28 @@
 from product import Product
+import unittest
 
-def test_calculate_total():
-    pdt = Product("x",12,2)
-    result = pdt.calculate_total()
-    return result == 24
+class TestProduct(unittest.TestCase):
+    def test_calculate_total(self):
+        pdt = Product("x",12,2)
+        result = pdt.calculate_total()
+        self.assertEqual(result,24)
 
-def test_zero_quantity():
-    pdt = Product("x",0,1)
-    try:
-        pdt.calculate_total()
-        return False
-    except ValueError:
-        return True
+    def test_zero_quantity(self):
+        pdt = Product("x",0,1)
+        with self.assertRaises(ValueError):
+            total = pdt.calculate_total()
+        
 
-def test_negative_quantity():
-    pdt = Product('x',-1,5)
-    try:
-        pdt.calculate_total()
-        return False
-    except ValueError:
-        return True
+    def test_negative_quantity(self):
+        pdt = Product('x',-1,5)
+        with self.assertRaises(ValueError):
+            total = pdt.calculate_total()
+        
+    def test_negative_price(self):
+        pdt = Product('x',9,-1)
+        with self.assertRaises(ValueError):
+            total = pdt.calculate_total()
 
-def test_negative_price():
-    pdt = Product('x',9,-1)
-    try:
-        pdt.calculate_total()
-        return False
-    except ValueError:
-        return True
-
-
-
-print(test_calculate_total())
-print(test_negative_price())
-print(test_negative_quantity())
-print(test_zero_quantity())
+# test class ......
+if __name__ == '__main__':
+    unittest.main()
--- test_store.py
+++ test_store.py
@@ -1,23 +1,20 @@
-
+import unittest
 from product import Product
 from store import ShopingCart
 
-class TestShopingCart:
+class TestShopingCart(unittest.TestCase):
     def test_getChart(self):
         cart = ShopingCart()
         total = cart.getCartTotal()
-        return total == 0
+        self.assertEqual(total,0)
     
     def test_getCartTotal_with_products(self):
         cart = ShopingCart()
         cart.addProduct(Product("x",12,3))
         cart.addProduct(Product("x",5,4))
         total = cart.getCartTotal()
-        return total == 2
+        self.assertEqual(total,2)
 
 # test class ......
-test = TestShopingCart()
-if test.test_getChart() and test.test_getCartTotal_with_products():
-    print(True)
-else:
-    print(False)
+if __name__ == '__main__':
+    unittest.main()
